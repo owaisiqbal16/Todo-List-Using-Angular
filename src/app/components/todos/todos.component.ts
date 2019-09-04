f8833e4430318978413fbb6cd0b25259da143517
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2019-09-05 00:04:33 +0500

    Basic todo stucture completed

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+import { TodoService, TodosService } from '../../services/todos.service';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos: Todo[];
+
+  constructor(private todoService: TodosService) { }
+
+  ngOnInit() {
+    this.todos = this.todoService.getTodos();
+  }
+
+}
